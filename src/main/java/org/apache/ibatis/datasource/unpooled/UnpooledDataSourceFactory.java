961ba7a05862883d90f5df376a8ea5572ce52e61
JinJunLiang <403216554@qq.com>
2019-01-03 15:49:23 +0800

    完成 不使用连接池的 DataSource 的代码的解析

--- src/main/java/org/apache/ibatis/datasource/unpooled/UnpooledDataSourceFactory.java
+++ src/main/java/org/apache/ibatis/datasource/unpooled/UnpooledDataSourceFactory.java
@@ -27,6 +27,10 @@ import org.apache.ibatis.reflection.SystemMetaObject;
 /**
  * @author Clinton Begin
  */
+/*
+* 该数据源的实现只是每次被请求打开和关闭连接，对于在数据库连接可用性方面没有太高要求
+* 的简单程序来说是挺好的
+* */
 public class UnpooledDataSourceFactory implements DataSourceFactory {
 
   private static final String DRIVER_PROPERTY_PREFIX = "driver.";
@@ -41,17 +45,24 @@ public class UnpooledDataSourceFactory implements DataSourceFactory {
   @Override
   public void setProperties(Properties properties) {
     Properties driverProperties = new Properties();
+    // 创建 DataSource 对应的 反射缓存对象
     MetaObject metaDataSource = SystemMetaObject.forObject(dataSource);
+    // 遍历 properties 属性，初始化到 driverProperties 和 MetaObject 中
     for (Object key : properties.keySet()) {
       String propertyName = (String) key;
+      // 假如属性是以 driver 开头的，就初始化到 driverProperties中
       if (propertyName.startsWith(DRIVER_PROPERTY_PREFIX)) {
         String value = properties.getProperty(propertyName);
+        // 去掉 driver. 之后存入
         driverProperties.setProperty(propertyName.substring(DRIVER_PROPERTY_PREFIX_LENGTH), value);
       } else if (metaDataSource.hasSetter(propertyName)) {
+        // 假如该对象有 setter 方法，就set 进去
         String value = (String) properties.get(propertyName);
+        // 找到该属性名对应的类型并转换为该类型的值
         Object convertedValue = convertValue(metaDataSource, propertyName, value);
         metaDataSource.setValue(propertyName, convertedValue);
       } else {
+        //其它的都是无效的属性
         throw new DataSourceException("Unknown DataSource property: " + propertyName);
       }
     }
@@ -65,8 +76,13 @@ public class UnpooledDataSourceFactory implements DataSourceFactory {
     return dataSource;
   }
 
+  /*
+  * value 是 字符串，但是 MetaObject 里面对应的值不一定是 String, 所以利用该方法看看该属性对应的类型是什么
+  * 并将value转换为该值
+  * */
   private Object convertValue(MetaObject metaDataSource, String propertyName, String value) {
     Object convertedValue = value;
+    // 获取 set 方法的属性
     Class<?> targetType = metaDataSource.getSetterType(propertyName);
     if (targetType == Integer.class || targetType == int.class) {
       convertedValue = Integer.valueOf(value);
